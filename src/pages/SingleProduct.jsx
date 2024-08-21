b6cf4d7436db88cfb99ca8e5cea22c8261036b4e
fahrurozi16 <muhammadfahrurozi518@gmail.com>
2024-08-21 09:19:50 +0700

    menambahkan feature dan store

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router-dom'
 import { formatPrice, customFetch, generateAmountOptions } from '../utils'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addItem } from '../features/cart/cartSlice'
 
 export const loader = async ({ params }) => {
     const response = await customFetch(`/products/${params.id}`)
@@ -21,6 +23,20 @@ const SingleProduct = () => {
 
     const handleAmount = (e) => {
         setAmount(parseInt(e.target.value))
+    }
+
+    const dispatch = useDispatch();
+    const cartProduct = {
+        cartID: product.id + productColor,
+        productID: product.id,
+        image,
+        title,
+        company,
+        productColor,
+    }
+
+    const addToCart = () => {
+        dispatch(addItem({ product: cartProduct }));
     }
 
     return (
@@ -67,7 +83,7 @@ const SingleProduct = () => {
                     </div>
                     {/* CART BTN */}
                     <div className="mt-10">
-                        <button className='btn btn-secondary btn-md' onClick={() => console.log('add to bag')}>Add to Bag</button>
+                        <button className='btn btn-secondary btn-md' onClick={addToCart}>Add to Bag</button>
                     </div>
                 </div>
 
